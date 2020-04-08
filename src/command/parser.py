9997c0f067e8568b1b505a0ebaf33fd20c1e256f
Cube <46578659+cnuebred@users.noreply.github.com>
2020-04-09 00:04:04 +0200

    add container

--- src/command/parser.py
+++ src/command/parser.py
@@ -1,6 +1,13 @@
+import pandas as pd
+import numpy as np
 
-async def mainParser(bot, msg):
+async def mainNode(bot, msg):
     params = msg.content.split(' ')
     command = (list(params)[0])[1:]
-    print(command)
-    print(container.container['user'][command])
+    container = pd.read_csv('src/command/container.csv')
+    if not(command in list(container['command'])):return print('I cant find this command')
+    commandModule = container[container['command'] == command].values[0]
+    print()
+
+async def mainParser(bot, msg):
+    getCommandModule = await mainNode(bot, msg)
